6df79fda6924bc1cd958553672f4fa202512554e
michaelford94 <mikeesmail1234@gmail.com>
2020-10-27 17:11:23 -0600

    10.1 10/27 /movies/:pk

--- index.js
+++ index.js
@@ -18,4 +18,27 @@ app.get("/movies", (req,res)=>{
         res.status(500).send()
     })
 })
+
+app.get("/movies/:pk", (req, res)=>{
+    var pk = req.params.pk
+    // console.log("my pk: ", pk)
+
+    var myQuery = `SELECT* FROM Movie
+    LEFT JOIN Genre ON genre.GenrePK = Movie.GenreFK
+    WHERE MoviePK = ${pk}`
+
+    db.executeQuery(myQuery)
+        .then((movies)=>{
+            // console.log("movies: ", movies)
+
+            if(movies[0]){
+                res.send(movies[0])
+
+            } else{res.status(404).send('bad request')}
+        })
+        .catch((err)=>{
+            console.log("Error in /movie/pk", err)
+            res.status(500).send()
+        })
+})
 app.listen(5000, ()=>{console.log("app is running on port 5000")})
