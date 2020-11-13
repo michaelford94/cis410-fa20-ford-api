021a638e5261eefef9a8f9921503b37f4cd33f3e
michaelford94 <mikeesmail1234@gmail.com>
2020-11-13 04:39:27 -0700

    Project updates

--- Project.js
+++ Project.js
@@ -19,143 +19,114 @@ app.use(cors())
 //TEST
 app.get("/", (req,res)=>{res.send("Hello world.")})
 
-
 //QUESTION 1 -- What is the link to GET the data entity?
-// app.get("/Location", (req,res)=>{
-//     //get data from database
-//     db.executeQuery(`SELECT *
-//     FROM Location
-//     LEFT JOIN BikeType
-//     ON BikeType.ProductPK = Location.ProductPK`)
-//     .then((result)=>{
-//         res.status(200).send(result)
-//     })
-//     .catch((err)=>{
-//         console.log(err);
-//         res.status(500).send()
-//     })
-// })
-
-
+app.get("/Workplace", (req,res)=>{
+    //get data from database
+    db.executeQuery(`SELECT *
+    FROM Workplace
+    LEFT JOIN Industry
+    ON Industry.IndustryPK = Workplace.WorkplacePK`)
+    .then((result)=>{
+        res.status(200).send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).send()
+    })
+})
 
 //QUESTION 2 -- What is the link to GET a particular record in your data entity?
-// app.get("/Location/:pk", (req, res)=>{
-//     var pk = req.params.pk
-//     // console.log("my PK:" , pk)
+app.get("/Workplace/:pk", (req, res)=>{
+    var pk = req.params.pk
+    // console.log("my PK:" , pk)
 
-//     var myQuery = 
-//     `SELECT *
-//     FROM Location
-//     LEFT JOIN BikeType
-//     ON BikeType.ProductPK = Location.ProductPK 
-//     WHERE LocationPK = ${pk}`
+    var myQuery = 
+    `SELECT *
+    FROM Workplace
+    LEFT JOIN Industry
+    ON Industry.IndustryPK = Workplace.WorkplacePK
+    WHERE WorkplacePK = ${pk}`
 
-//     db.executeQuery(myQuery)
-//         .then((Location)=>{
-//             // console.log("Movies: ", movies)
+    db.executeQuery(myQuery)
+        .then((Workplace)=>{
 
-//             if(Location[0]){
-//                 res.send(Location[0])
-//             }else{res.status(404).send('bad request')}
-//         })
-//         .catch((err)=>{
-//             console.log("Error in /Location/pk", err)
-//             res.status(500).send()
-//         })
-// })
-
-
+            if(Workplace[0]){
+                res.send(Workplace[0])
+            }else{res.status(404).send('bad request')}
+        })
+        .catch((err)=>{
+            console.log("Error in /Workplace/pk", err)
+            res.status(500).send()
+        })
+})
 
 //QUESTION 3 -- What is the link to POST a new user?
+app.post("/JobSeeker", async (req,res)=>{
+    // res.send("creating user")
+    // console.log("request body", req.body)
 
-// app.post("/contacts", async (req,res)=>{
-//     // res.send("creating user")
-//     // console.log("request body", req.body)
+    var nameFirst = req.body.nameFirst;
+    var nameLast = req.body.nameLast;
+    var email = req.body.email;
+    var password = req.body.password;
 
-//     var nameFirst = req.body.nameFirst;
-//     var nameLast = req.body.nameLast;
-//     var email = req.body.email;
-//     var password = req.body.password;
+    if(!nameFirst || !nameLast || !email || !password){
+        return res.status(400).send("bad request")
+    }
 
-//     //Validation to make sure all fields are provided
-//     if(!nameFirst || !nameLast || !email || !password){
-//         return res.status(400).send("bad request")
-//     }
+    nameFirst = nameFirst.replace("'","''")
+    nameLast = nameLast.replace("'","''")
 
-//     nameFirst = nameFirst.replace("'","''")
-//     nameLast = nameLast.replace("'","''")
+    //Checking for exisitng email
+    var emailCheckQuery = `SELECT Email
+    FROM JobSeeker
+    WHERE Email = '${email}'`
 
-//     var emailCheckQuery = `SELECT email
-//     FROM contact
-//     WHERE email = '${email}'`
+    var existingUser = await db.executeQuery(emailCheckQuery)
 
-//     var existingUser = await db.executeQuery(emailCheckQuery)
+    if(existingUser[0]){
+        return res.status(409).send('Please enter a different email.')
+    }
 
-//     //This prevents users from registering with a duplicate email
-//     if(existingUser[0]){
-//         return res.status(409).send('Please enter a different email.')
-//     }
+    //Hashed password
+    var hashedPassword = bcrypt.hashSync(password)
 
-//     //Password is hashed
-//     var hashedPassword = bcrypt.hashSync(password)
+    var insertQuery = `INSERT INTO JobSeeker(NameFirst,NameLast,Email,Password)
+    VALUES('${nameFirst}','${nameLast}','${email}','${hashedPassword}')`
 
-//     var insertQuery = `INSERT INTO contact(NameFirst,NameLast,Email,Password)
-//     VALUES('${nameFirst}','${nameLast}','${email}','${hashedPassword}')`
-
-//     //New user is written to database
-//     db.executeQuery(insertQuery)
-
-//         //sends a sucessfull 201 repsonse
-//         .then(()=>{res.status(201).send()})
-//         .catch((err)=>{
-//             console.log("error in POST /contacts",err)
-//             res.status(500).send()
-//         })
-// })
-
-
+    //New user written to database
+    db.executeQuery(insertQuery)
+    
+        //Sends sucessfull 201 response
+        .then(()=>{res.status(201).send()})
+        .catch((err)=>{
+            console.log("error in POST /JobSeeker",err)
+            res.status(500).send()
+        })
+})
 
 //QUESTION 4 -- What is the POST API to login a user? 
 
 
 
 //QUESTION 5 -- What is the POST link to logout a user?
+app.post('/JobSeeker/logout', auth, (req,res)=>{
+    var query = `UPDATE JobSeeker
+    SET Token = NULL
+    WHERE JobSeekerPK = ${req.contact.ContactPK}`
 
-
+    db.executeQuery(query)
+        .then(()=>{res.status(200).send()})
+        .catch((error)=>{
+            console.log("error in POST /JobSeeker/logout", error)
+            res.status(500).send()
+    })
+})
 
 //QUESTION 6 -- What is the POST link to create a transaction?
-app.post("/TransactType", auth, async (req,res)=>{
 
-//Route protected by authenitcated middleware in code above
 
-    try{ 
-        var ProductPK = req.body.ProductPK;
-        var OrderNumber = req.body.OrderNumber;
-        var Date = req.body.Date;
-        var Quantity = req.body.Quantity;
-    
-        //Make sure all routes are required
-        if(!ProductPK || !OrderNumber || !Date || !Quantity){res.status(400).send("bad request")}
 
-        OrderNumber = OrderNumber.replace("'","''")
-    
-        // console.log("here is the contact in /reviews",req.contact)
-        // res.send("here is your response")
-
-        let insertQuery = `INSERT INTO TransactType(ProductPK, OrderNumber, Date, Quantity, ContactFK)
-        OUTPUT inserted.ProductPK, inserted.OrderNumber, inserted.Date, inserted.Quantity
-        VALUES('${ProductPK}','${OrderNumber}','${Date}', '${Quantity}', ${req.contact.ContactPK})`
-
-        let insertedTransact = await db.executeQuery(insertQuery)
-
-        //Record is sucessfully written to database
-        res.status(201).send(insertedTransact[0])
-    }
-    catch(error){
-        console.log("error in POST /TransactType", error);
-        res.status(500).send()
-    }
-})
 
 //QUESTION 7 -- What is the GET route to get all transactions (events/orders) records for a user?
 
