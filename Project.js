0577641cb205ad2875221d2f1f15b590c10023fa
Mgilmore2020 <believe2@colostate.edu>
2020-11-15 20:06:12 -0700

    project closing

--- Project.js
+++ Project.js
@@ -61,91 +61,173 @@ app.get("/Workplace/:pk", (req, res)=>{
 })
 
 //QUESTION 3 -- What is the link to POST a new user?
-<<<<<<< HEAD
-
-app.post("/customers", async (req,res)=>{
-=======
-app.post("/JobSeeker", async (req,res)=>{
->>>>>>> 021a638e5261eefef9a8f9921503b37f4cd33f3e
-    // res.send("creating user")
-    // console.log("request body", req.body)
-
-    var nameFirst = req.body.nameFirst;
-    var nameLast = req.body.nameLast;
-    var email = req.body.email;
-    // var password = req.body.password;
-
-<<<<<<< HEAD
-    //Validation to make sure all fields are provided
-    if(!nameFirst || !nameLast || !email){
-=======
-    if(!nameFirst || !nameLast || !email || !password){
->>>>>>> 021a638e5261eefef9a8f9921503b37f4cd33f3e
-        return res.status(400).send("bad request")
-    }
-
-    nameFirst = nameFirst.replace("'","''")
-    nameLast = nameLast.replace("'","''")
-
-<<<<<<< HEAD
-    var emailCheckQuery = `SELECT email
-    FROM Customer
-=======
-    //Checking for exisitng email
-    var emailCheckQuery = `SELECT Email
-    FROM JobSeeker
->>>>>>> 021a638e5261eefef9a8f9921503b37f4cd33f3e
-    WHERE Email = '${email}'`
-
-    var existingUser = await db.executeQuery(emailCheckQuery)
-
-    if(existingUser[0]){
-        return res.status(409).send('Please enter a different email.')
-    }
-
-    //Hashed password
-    var hashedPassword = bcrypt.hashSync(password)
-
-    var insertQuery = `INSERT INTO JobSeeker(NameFirst,NameLast,Email,Password)
-    VALUES('${nameFirst}','${nameLast}','${email}','${hashedPassword}')`
-
-    //New user written to database
-    db.executeQuery(insertQuery)
+    app.post("/jobseeker", async (req,res)=>{
+        //res.send("creating jobseeker");
+        //console.log("request body", req.body)
     
-        //Sends sucessfull 201 response
-        .then(()=>{res.status(201).send()})
-        .catch((err)=>{
-            console.log("error in POST /JobSeeker",err)
-            res.status(500).send()
-        })
-})
+        var nameFirst= req.body.nameFirst;
+        var nameLast = req.body.nameLast;
+        var email = req.body.email;
+        var password = req.body.password;
+    
+        if(!nameFirst || !nameLast || !email || !password){
+            return res.status(400).send("Bad Request")
+        }
+    
+        nameFirst = nameFirst.replace("'","''")
+        nameLast = nameLast.replace("'","''")
+    
+        var emailCheckQuery = `SELECT Email
+        FROM JobSeeker
+        WHERE Email = '${email}'`;
+    
+        var existingUser = await db.executeQuery(emailCheckQuery);
+    
+        //console.log("existing user", existingUser)
+        if(existingUser[0]){
+            return res.status(409).send("Please enter a different email")
+        }
+    
+        var hashedPassword = bcrypt.hashSync(password)
+        var insertQuery = `INSERT INTO JobSeeker(NameFirst,NameLast,Email,Password)
+        VALUES('${nameFirst}','${nameLast}', '${email}','${hashedPassword}')
+        `
+    
+        db.executeQuery(insertQuery)
+            .then(()=>{res.status(201).send()})
+            .catch((err)=>{
+                console.log("error in POST /jobseeker",err)
+                res.status(500).send()
+            })
+    })
 
 //QUESTION 4 -- What is the POST API to login a user? 
 
+app.post("/jobseeker/login", async (req,res)=>{
+    //console.log(req.body)
 
+    var email = req.body.email;
+    var password = req.body.password;
+
+    if(!email || !password){
+        return res.status(400).send("Bad Request");
+    }
+
+    //1. check that user email exists in the database
+    var query = `SELECT *
+    FROM JobSeeker 
+    WHERE Email = '${email}'`
+
+    
+    let result;
+
+    try{
+        result = await db.executeQuery(query)
+    }catch(myError){
+        console.log("Error in /jobseeker/login:", myError);
+        return res.status(500).send();
+    }
+
+    // console.log(result);
+
+    if(!result[0]){
+        return res.status(400).send("Invalid User Credentials")
+    }
+
+    //2. check their password  
+
+    let user = result[0];
+    // console.log(user);
+
+    if(!bcrypt.compareSync(password, user.Password)){
+        console.log("Invalid Password");
+        return res.status(400).send("Invalid User Credentials");
+    }
+    //3. generate a token
+    
+    let token = jwt.sign({pk: user.JobSeekerPK}, config.JWT, {expiresIn: "60 minutes"})
+
+    //console.log(token)
+
+    //4. Save token in db and send token and user info back to user 
+    let setTokenQuery = `UPDATE JobSeeker
+    SET Token = '${token}'
+    WHERE JobSeekerPK = ${user.JobSeekerPK}`
+
+    try{
+        await db.executeQuery(setTokenQuery)
+
+        res.status(200).send({
+            token: token,
+            user: {
+                NameFirst: user.NameFirst,
+                NameLast: user.NameLast,
+                Email: user.Email,
+                JobSeekerPK: user.JobSeekerPK
+            }
+        })
+    }
+    catch(myError){
+        console.log("error setting user token", myError);
+        res.status(500).send()
+    }
+
+})
 
 //QUESTION 5 -- What is the POST link to logout a user?
-app.post('/JobSeeker/logout', auth, (req,res)=>{
+app.post("/jobseeker/logout", auth, (req,res)=>{
     var query = `UPDATE JobSeeker
     SET Token = NULL
-    WHERE JobSeekerPK = ${req.contact.ContactPK}`
+    WHERE JobSeekerPK = ${req.jobseeker.JobSeekerPK}`
 
     db.executeQuery(query)
         .then(()=>{res.status(200).send()})
         .catch((error)=>{
-            console.log("error in POST /JobSeeker/logout", error)
+            console.log("error in POST /jobseeker/logout", error)
             res.status(500).send()
-    })
+        })
 })
 
 //QUESTION 6 -- What is the POST link to create a transaction?
 
+app.post("/applications", auth, async (req,res)=>{
 
+    try{
+        var workplaceFK = req.body.workplaceFK;
+        var about = req.body.about;
+        var dateApplied = req.body.dateApplied;
+    
+        if(!workplaceFK || !about || !dateApplied){res.status(400).send("Bad Request")}
+
+        about = about.replace("'","''")
+    
+        // console.log("Here is the jobseeker in /applications", req.jobseeker)
+        // res.send("Here is your response")
+
+        let insertQuery = `INSERT INTO Application(About, DateApplied, WorkplaceFK, JobSeekerFK)
+        OUTPUT inserted.ApplicationPK, inserted.About, inserted.DateApplied, inserted.JobSeekerFK
+        VALUES('${about}', '${dateApplied}', ${workplaceFK}, ${req.jobseeker.JobSeekerPK})`
+
+        let insertedApplication = await db.executeQuery(insertQuery)
+        
+        
+        //console.log(insertedApplication);
+        
+        
+        res.status(201).send(insertedApplication[0])    
+    }
+    catch(error){
+        console.log("error in POST /applications", error)
+        res.status(500).send();
+    } 
+})
 
 
 //QUESTION 7 -- What is the GET route to get all transactions (events/orders) records for a user?
 
-
+app.get("/jobseeker/me", auth, (req,res)=>{
+    res.send(req.jobseeker)
+})
 
 
 const PORT = process.env.PORT || 5000
